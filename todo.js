bd9c78d8febcc657a5edccb445958900c140d7c3
T.Mabotja <mabatshegofatso842@gmail.com>
2026-03-11 23:52:41 +0200

    another change

--- todo.js
+++ todo.js
@@ -1,18 +1,17 @@
 let todo = document.getElementById(`item`);
 const addBtn = document.getElementById(`add`);
 const removeBtn= document.getElementById(`remove`);
+let li = document.createElement("li");
+
 
 addBtn.addEventListener("click", function(){
-    console.log("yes")
     let list = document.getElementById("list");
-    let li = document.createElement("li");
     li.textContent=todo.value;
     list.appendChild(li)
     todo.value ="";
 })
 removeBtn.addEventListener(`click`, function(){
     let list = document.getElementById(`list`);
-    let li = document.createElement(`li`);
     li.textContent=todo.value;
     if(li in list){
         list.removeChild(li)
